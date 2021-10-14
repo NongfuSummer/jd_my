88b22773418349317de12fbf316f68b0b128aed2
lhy0315cn <65433261+lhy0315cn@users.noreply.github.com>
2021-10-14 23:42:05 +0800

    Update wx_yyd.js

--- wx_yyd.js
+++ wx_yyd.js
@@ -83,7 +83,7 @@ appyq = process.env.appyq;
   } else{
   app_soy_v_yyd_authorization.push($.getdata('soy_v_yyd_authorization'))
   //app_soy_v_yyd_User_Agent.push($.getdata('oy_v_yyd_User_Agent'))
-   User_Agent=$.getdata('oy_v_yyd_User_Agent')
+   User_Agent=$.getdata('soy_v_yyd_User_Agent')
     if(User_Agent=="undefined"||User_Agent==''){
         User_Agent='Mozilla/5.0 (Linux; Android 9; PDBM00 Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/89.0.4389.72 MQQBrowser/6.2 TBS/045811 Mobile Safari/537.36 MMWEBID/7016 MicroMessenger/8.0.11.1980(0x28000B51) Process/tools WeChat/arm64 Weixin NetType/WIFI Language/zh_CN ABI/arm64'
     }
